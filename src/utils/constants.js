516c7c1ed999c0639b9170b62dd9322a8f252586
johnstewart0820 <john.s@outsource.codes>
2021-02-19 11:03:18 +0300

    Modify: Fix typo in constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ class constants {
     ENTER_VALID_EMAIL = "Proszę wprowadzić poprawny adres e-mail.";
     ENTER_PASSWORD = "Hasło musi mmieć co namniej 6 znaków.";
     ENTER_SAME_PASSWORD = "Hasła muszą być takie same.";
-    CHECk_POLICY = "Proszę zaakceptować regulamin.";
+    CHECK_POLICY = "Proszę zaakceptować regulamin.";
     unauthenticated_url = [
         '/not-found',
         '/login',

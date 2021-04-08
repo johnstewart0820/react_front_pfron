4455742ffa6739e8aff2713beb7bbaf987f967aa
johnstewart0820 <john.s@outsource.codes>
2021-04-08 13:39:04 +0300

    Add: PDF download feature

--- src/views/IprPlan/IprPlan.js
+++ src/views/IprPlan/IprPlan.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import useStyles from './style';
 import { Alert } from 'components';
 import {
@@ -24,17 +24,18 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import ExpandMoreOutlinedIcon from '@material-ui/icons/ExpandMoreOutlined';
 import ExpandLessOutlinedIcon from '@material-ui/icons/ExpandLessOutlined';
 import domtopdf from 'dom-to-pdf';
+import ReactToPdf from 'react-to-pdf';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import './tab.css';
 
-import {PlanView, ScheduleView} from './components';
+import { PlanView, ScheduleView } from './components';
 
 const IprPlan = props => {
 	const id = props.match.params.id;
 	const [openModal, setOpenModal] = useState(false);
 	const { history } = props;
 	const classes = useStyles();
-	
+
 	const breadcrumbs = [{ active: true, label: 'Uczestnicy', href: '/participants' }, { active: true, label: 'Lista IPR', href: '/ipr_list' }, { active: false, label: 'Plan realizacji IPR' }];
 	const [show_status, setShowStatus] = useState(false);
 	const [moduleList, setModuleList] = useState([]);
@@ -58,12 +59,13 @@ const IprPlan = props => {
 	const [status, setStatus] = useState([]);
 	const [dateList, setDateList] = useState([]);
 	const [selectedScheduleItem, setSelectedScheduleItem] = useState(0);
-  const plan_ref = useRef(null);
+	const plan_ref = useRef(null);
 	const schedule_ref = useRef(null);
-		const [hasAlert, setHasAlert] = useState(false);
+	const ref = useRef(null);
+	const [hasAlert, setHasAlert] = useState(false);
 	const [isSuccess, setIsSuccess] = useState(false);
 	const [message, setMessage] = useState('');
-        const [progressStatus, setProgressStatus] = useState(false);
+	const [progressStatus, setProgressStatus] = useState(false);
 	const [error, setError] = useState({});
 
 	useEffect(() => {
@@ -73,18 +75,18 @@ const IprPlan = props => {
 					history.push('/login');
 				} else {
 					let _arr = [];
-					for (let i = 0; i < response.data.module.length; i ++) {
+					for (let i = 0; i < response.data.module.length; i++) {
 						let item = response.data.module[i];
 						item.plan = [];
-						for (let j = 0; j < response.data.plan.length; j ++) {
+						for (let j = 0; j < response.data.plan.length; j++) {
 							if (response.data.module[i].id === response.data.plan[j].module) {
 								response.data.plan[j].new = 'false';
 								if (response.data.plan[j].start_date === null) {
 									let _date = new Date();
-									response.data.plan[j].start_date = _date.getFullYear() + '-' + (_date.getMonth() + 1) + '-' +_date.getDate()
+									response.data.plan[j].start_date = _date.getFullYear() + '-' + (_date.getMonth() + 1) + '-' + _date.getDate()
 								} else {
 									let _date = new Date(response.data.plan[j].start_date);
-									response.data.plan[j].start_date = _date.getFullYear() + '-' + (_date.getMonth() + 1) + '-' +_date.getDate()
+									response.data.plan[j].start_date = _date.getFullYear() + '-' + (_date.getMonth() + 1) + '-' + _date.getDate()
 								}
 								item.plan.push(response.data.plan[j]);
 							}
@@ -94,7 +96,7 @@ const IprPlan = props => {
 					setModuleList(_arr);
 				}
 			})
-		
+
 		ipr.getInfo()
 			.then(response => {
 				if (response.code === 401) {
@@ -106,7 +108,7 @@ const IprPlan = props => {
 			})
 	}, []);
 
-	
+
 	useEffect(() => {
 		ipr.get(id)
 			.then(response => {
@@ -115,7 +117,7 @@ const IprPlan = props => {
 				} else {
 					setParticipant(response.data.ipr.id_candidate);
 					getOrkPersonData(response.data.ipr.id_candidate, response.data.ipr.id_ork_person);
-					for (let i = 0; i < participantList.length; i ++) {
+					for (let i = 0; i < participantList.length; i++) {
 						if (participantList[i].id === response.data.ipr.id_candidate) {
 							setParticipantName(participantList[i].name + ' ' + participantList[i].surname);
 							setParticipantNumber(participantList[i].participant_number);
@@ -134,20 +136,20 @@ const IprPlan = props => {
 		const format2 = "YYYY-MM-DD";
 		let _date = moment(date).format(format2);
 		ipr.getScheduleData(id, _date)
-		.then(response => {
-			if (response.code === 401) {
-				history.push('/login');
-			} else {
-				let _schedule_data = response.data.module;
-				for (let i = 0; i < _schedule_data.length; i ++) {
-					for (let j = 0; j < _schedule_data[i].service_list.length; j ++) {
-						if (!_schedule_data[i].service_list[j].schedule)
-							_schedule_data[i].service_list[j].schedule = {start_time: '00:00', end_time: '00:00', break_time: '0', total_time: '0', total_amount: '0'}
+			.then(response => {
+				if (response.code === 401) {
+					history.push('/login');
+				} else {
+					let _schedule_data = response.data.module;
+					for (let i = 0; i < _schedule_data.length; i++) {
+						for (let j = 0; j < _schedule_data[i].service_list.length; j++) {
+							if (!_schedule_data[i].service_list[j].schedule)
+								_schedule_data[i].service_list[j].schedule = { start_time: '00:00', end_time: '00:00', break_time: '0', total_time: '0', total_amount: '0' }
+						}
 					}
+					setScheduleData(_schedule_data);
 				}
-				setScheduleData(_schedule_data);
-			}
-		})
+			})
 	}
 
 	const getOrkPersonData = (data, id_ork_person) => {
@@ -158,7 +160,7 @@ const IprPlan = props => {
 				} else {
 					setNumber(response.data.count);
 					setOrkPersonList(response.data.ork_person);
-					for (let i = 0; i < response.data.ork_person.length; i ++) {
+					for (let i = 0; i < response.data.ork_person.length; i++) {
 						if (response.data.ork_person[i].id == id_ork_person)
 							setOrkPerson(response.data.ork_person[i]);
 					}
@@ -171,9 +173,9 @@ const IprPlan = props => {
 	}
 
 	const checkScheduleData = (scheduleData) => {
-		for (let i = 0; i < scheduleData.length; i ++) {
+		for (let i = 0; i < scheduleData.length; i++) {
 			let service_list = scheduleData[i].service_list;
-			for (let j = 0; j < service_list.length; j ++) {
+			for (let j = 0; j < service_list.length; j++) {
 				if (service_list[j].schedule.error === true || service_list[j].error_amount === true)
 					return false;
 			}
@@ -182,7 +184,7 @@ const IprPlan = props => {
 	}
 
 	const handleSave = () => {
-     
+
 		if (selectedItem == 0) {
 			setProgressStatus(true);
 			ipr.updatePlan(moduleList, id)
@@ -191,8 +193,8 @@ const IprPlan = props => {
 						history.push('/login');
 					} else {
 						setHasAlert(true);
-					setMessage(response.message);
-					setIsSuccess(response.code === 200);
+						setMessage(response.message);
+						setIsSuccess(response.code === 200);
 						if (response.code === 200) {
 							setTimeout(function () { history.push('/ipr_list'); }, 1000);
 						}
@@ -216,8 +218,8 @@ const IprPlan = props => {
 						history.push('/login');
 					} else {
 						setHasAlert(true);
-					setMessage(response.message);
-					setIsSuccess(response.code === 200);
+						setMessage(response.message);
+						setIsSuccess(response.code === 200);
 						if (response.code === 200) {
 							// setTimeout(function () { history.push('/ipr_list'); }, 1000);
 						}
@@ -228,7 +230,7 @@ const IprPlan = props => {
 	}
 
 	const handleDelete = () => {
-    
+
 		setProgressStatus(true);
 		ipr
 			.delete(id)
@@ -254,12 +256,12 @@ const IprPlan = props => {
 	const handleExportPdf = () => {
 		const plan_dom = plan_ref.current;
 		const schedule_dom = schedule_ref.current;
-    var options = {
-      filename: 'download.pdf',
-    };
-		
-    domtopdf(selectedItem === 0 ? plan_dom : schedule_dom, options, function() {
-    });
+		var options = {
+			filename: 'download.pdf',
+		};
+
+		domtopdf(selectedItem === 0 ? plan_dom : schedule_dom, options, function () {
+		});
 	}
 
 	const toggleView = () => {
@@ -272,176 +274,176 @@ const IprPlan = props => {
 
 	return (
 		<MuiPickersUtilsProvider utils={DateFnsUtils} locale={pl}>
-		<div className={classes.public}>
-			<div className={classes.controlBlock}>
-				<Breadcrumb list={breadcrumbs} />
-				<Button variant="outlined" color="secondary" id="main"  className={classes.btnBack} onClick={handleBack}>					Wróć do listy IPR
+			<div className={classes.public}>
+				<div className={classes.controlBlock}>
+					<Breadcrumb list={breadcrumbs} />
+					<Button variant="outlined" color="secondary" id="main" className={classes.btnBack} onClick={handleBack}>					Wróć do listy IPR
 				</Button>
-			</div>
-			<Alert 
+				</div>
+				<Alert
 					hasAlert={hasAlert}
 					setHasAlert={setHasAlert}
 					isSuccess={isSuccess}
 					message={message}
 				/>
-			<Grid container spacing={3} className={classes.formBlock}>
-				<Grid item md={9} xs={12}>
-					<Card className={classes.form}>
-						<Grid container spacing={3}>
-							<Grid item xs={12} className={classes.form_title} >
-								<div style={{display: 'flex', justifyContent: 'space-between'}}>
-									<div>Dane IPR</div>
-									<div onClick={toggleView}>
-										{ show_status ? <ExpandLessOutlinedIcon/> : <ExpandMoreOutlinedIcon/>}
-									</div>
-								</div>
-							</Grid>
-						</Grid>
-						{
-							show_status ?
+				<Grid container spacing={3} className={classes.formBlock}>
+					<Grid item md={9} xs={12}>
+						<Card className={classes.form}>
 							<Grid container spacing={3}>
-							<Grid item xs={3}></Grid>
-							<Grid item xs={9}>
-								<Grid container spacing={3}>
-									<Grid item xs={5}>
-										<div className={classes.top_label} htmlFor="name">Numer uczestnika</div>
-										<div className={classes.number}>{participant_number}</div>
-									</Grid>
-									<Grid item xs={7}>
-										<div className={classes.top_label} htmlFor="name">Uczestnik</div>
-										<div className={classes.number}>{participant_name}</div>
-									</Grid>
-									<Grid item xs={5}>
-										<div className={classes.top_label} htmlFor="name">Typ</div>
-										<SingleSelect value={ipr_type} list={iprTypeList} disabled={true}/>
-									</Grid>
-									<Grid item xs={2}>
-										<div className={classes.top_label} htmlFor="name">Numer</div>
-										<div className={classes.number}>{number}</div>
-									</Grid>
-									<Grid item xs={5}>
-										<div className={classes.top_label} htmlFor="name">Data wypełnienia</div>
-										<KeyboardDatePicker
-											disableToolbar
-											disabled={true}
-											variant="inline"
-											format="dd.MM.yyyy"
-											margin="normal"
-											id="date-picker-inline"
-											value={schedule_date}
-											onChange={setScheduleDate}
-											KeyboardButtonProps={{
-												'aria-label': 'change date',
-											}}
-										/>
-									</Grid>
-									<Grid item xs={12}>
-										<div className={classes.top_label} htmlFor="name">Specjalista ds. zarządzania rehabilitacją</div>
-										<Autocomplete
-											disabled={true}
-											className={classes.name_select_box}
-											onChange={(event, value) => setOrkPerson(value)}
-											value={ork_person}
-											options={orkPersonList}
-											getOptionLabel={(option) => participantList && option && option.name}
-											renderInput={(params) => <TextField {...params} variant="outlined" InputLabelProps={{ shrink: false }} />}
-										/>
-									</Grid>
-									<Grid item xs={12}>
-										<div className={classes.top_label} htmlFor="name">Wybrany zawód</div>
-										<input className={classes.input_box} type="name" value={profession} name="name" disabled={true}/>
-									</Grid>
+								<Grid item xs={12} className={classes.form_title} >
+									<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+										<div>Dane IPR</div>
+										<div onClick={toggleView}>
+											{show_status ? <ExpandLessOutlinedIcon /> : <ExpandMoreOutlinedIcon />}
+										</div>
+									</div>
 								</Grid>
 							</Grid>
+							{
+								show_status ?
+									<Grid container spacing={3}>
+										<Grid item xs={3}></Grid>
+										<Grid item xs={9}>
+											<Grid container spacing={3}>
+												<Grid item xs={5}>
+													<div className={classes.top_label} htmlFor="name">Numer uczestnika</div>
+													<div className={classes.number}>{participant_number}</div>
+												</Grid>
+												<Grid item xs={7}>
+													<div className={classes.top_label} htmlFor="name">Uczestnik</div>
+													<div className={classes.number}>{participant_name}</div>
+												</Grid>
+												<Grid item xs={5}>
+													<div className={classes.top_label} htmlFor="name">Typ</div>
+													<SingleSelect value={ipr_type} list={iprTypeList} disabled={true} />
+												</Grid>
+												<Grid item xs={2}>
+													<div className={classes.top_label} htmlFor="name">Numer</div>
+													<div className={classes.number}>{number}</div>
+												</Grid>
+												<Grid item xs={5}>
+													<div className={classes.top_label} htmlFor="name">Data wypełnienia</div>
+													<KeyboardDatePicker
+														disableToolbar
+														disabled={true}
+														variant="inline"
+														format="dd.MM.yyyy"
+														margin="normal"
+														id="date-picker-inline"
+														value={schedule_date}
+														onChange={setScheduleDate}
+														KeyboardButtonProps={{
+															'aria-label': 'change date',
+														}}
+													/>
+												</Grid>
+												<Grid item xs={12}>
+													<div className={classes.top_label} htmlFor="name">Specjalista ds. zarządzania rehabilitacją</div>
+													<Autocomplete
+														disabled={true}
+														className={classes.name_select_box}
+														onChange={(event, value) => setOrkPerson(value)}
+														value={ork_person}
+														options={orkPersonList}
+														getOptionLabel={(option) => participantList && option && option.name}
+														renderInput={(params) => <TextField {...params} variant="outlined" InputLabelProps={{ shrink: false }} />}
+													/>
+												</Grid>
+												<Grid item xs={12}>
+													<div className={classes.top_label} htmlFor="name">Wybrany zawód</div>
+													<input className={classes.input_box} type="name" value={profession} name="name" disabled={true} />
+												</Grid>
+											</Grid>
+										</Grid>
+									</Grid>
+									:
+									<></>
+							}
+						</Card>
+						<div className={classes.second_form} ref={ref}>
+							<Tabs onSelect={setSelectedItem}>
+								<TabList >
+									<Tab style={{ width: '50%' }}>PLAN REALIZACJI</Tab>
+									<Tab style={{ width: '50%' }}>HARMONOGRAMY TYGODNIOWE</Tab>
+								</TabList>
+								<TabPanel>
+									<div style={{ width: ref.current ? ref.current.clientWidth : '100%'}} ref={plan_ref}>
+										<PlanView
+											moduleList={moduleList}
+											setModuleList={setModuleList}
+										/>
+									</div>
+								</TabPanel>
+								<TabPanel>
+									<div style={{ width: ref.current ? ref.current.clientWidth : '100%'}} ref={schedule_ref}>
+										<ScheduleView
+											scheduleDate={schedule_date}
+											scheduleData={scheduleData}
+											setScheduleData={setScheduleData}
+											handleGetScheduleData={handleGetScheduleData}
+											week={week}
+											setWeek={setWeek}
+											weeks={weeks}
+											setWeeks={setWeeks}
+											status={status}
+											setStatus={setStatus}
+											dateList={dateList}
+											setDateList={setDateList}
+											selectedItem={selectedScheduleItem}
+											setSelectedItem={setSelectedScheduleItem}
+											id={id}
+										/>
+									</div>
+								</TabPanel>
+							</Tabs>
+						</div>
+
+					</Grid>
+					<Grid item md={3} xs={12}>
+						<Card className={classes.form}>
+							<Grid container spacing={3}>
+								<Grid item xs={4}>
+									<Button variant="outlined" color="secondary" className={classes.btnOption} onClick={handleExportPdf}>
+										<PictureAsPdfOutlinedIcon />
+									</Button>
+								</Grid>
+								<Grid item xs={4}>
+									<Button variant="outlined" color="secondary" className={classes.btnOption} onClick={handlePreview}>
+										<FindInPageOutlinedIcon />
+									</Button>
+								</Grid>
+								<Grid item xs={4}>
+									<Button variant="outlined" color="secondary" className={classes.btnOption} onClick={handleDelete}>
+										<DeleteIcon />
+									</Button>
+								</Grid>
+								<Grid item xs={12}>
+									<Button variant="outlined" color="secondary" className={classes.btnSave} onClick={handleSave}>
+										Zapisz
+									</Button>
+								</Grid>
 							</Grid>
-							:
-							<></>
-						}
-					</Card>
-					<div className={classes.second_form	}>
-						<Tabs onSelect={setSelectedItem}>
-							<TabList >
-								<Tab style={{width: '50%'}}>PLAN REALIZACJI</Tab>
-								<Tab style={{width: '50%'}}>HARMONOGRAMY TYGODNIOWE</Tab>
-							</TabList>
-							<TabPanel>
-								<div ref={plan_ref}>
-									<PlanView
-										moduleList={moduleList}
-										setModuleList={setModuleList}
-									/>
-								</div>
-							</TabPanel>
-							<TabPanel>
-								<div ref={schedule_ref}>
-									<ScheduleView
-										scheduleDate={schedule_date}
-										scheduleData={scheduleData}
-										setScheduleData={setScheduleData}
-										handleGetScheduleData={handleGetScheduleData}
-										week={week}
-										setWeek={setWeek}
-										weeks={weeks}
-										setWeeks={setWeeks}
-										status={status}
-										setStatus={setStatus}
-										dateList={dateList}
-										setDateList={setDateList}
-										selectedItem={selectedScheduleItem}
-										setSelectedItem={setSelectedScheduleItem}
-										id={id}
-									/>
-								</div>
-							</TabPanel>
-						</Tabs>
-					</div>
-					
+						</Card>
+					</Grid>
 				</Grid>
-				<Grid item md={3} xs={12}>
-					<Card className={classes.form}>
-						<Grid container spacing={3}>
-							<Grid item xs={4}>
-								<Button variant="outlined" color="secondary" className={classes.btnOption} onClick={handleExportPdf}>
-								<PictureAsPdfOutlinedIcon/>
-								</Button>
-							</Grid>
-							<Grid item xs={4}>
-								<Button variant="outlined" color="secondary" className={classes.btnOption} onClick={handlePreview}>
-								<FindInPageOutlinedIcon/>
-								</Button>
-							</Grid>
-							<Grid item xs={4}>
-								<Button variant="outlined" color="secondary" className={classes.btnOption} onClick={handleDelete}>
-								<DeleteIcon/>
-								</Button>
-							</Grid>
-							<Grid item xs={12}>
-								<Button variant="outlined" color="secondary" className={classes.btnSave} onClick={handleSave}>
-									Zapisz
-								</Button>
-							</Grid>
-						</Grid>
-					</Card>
-				</Grid>
-			</Grid>
-		</div>
-		{
-			progressStatus ?
-				<>
-					<div className={classes.progressContainer}>
-						<CircularProgress className={classes.progress} />
-					</div>
-				</>
-				:
-				<></>
-		}
-		<DeleteModal
+			</div>
+			{
+				progressStatus ?
+					<>
+						<div className={classes.progressContainer}>
+							<CircularProgress className={classes.progress} />
+						</div>
+					</>
+					:
+					<></>
+			}
+			<DeleteModal
 				openModal={openModal}
 				handleClose={handleCloseModal}
 				handleDelete={handleDelete}
 				selectedIndex={id}
 			/>
-	</MuiPickersUtilsProvider>
+		</MuiPickersUtilsProvider>
 	);
 };
 

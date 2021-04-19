229b5680b0aeb055f68aa1e300a5c78be9007003
johnstewart0820 <john.s@outsource.codes>
2021-04-19 12:47:06 +0300

    Add: Recuirtment report

--- src/apis/report.js
+++ src/apis/report.js
@@ -60,6 +60,27 @@ class Report {
 		}).catch(error => {
 				return error;
 		})
-}
+	}
+	getRecruitmentData = (rehabitation_center, quater) => {
+		return axios
+		.get(`${process.env.REACT_APP_BACKEND_URL}/report/recruitment_data`, {
+				headers: authHeader(storage.getStorage('token')),
+				params: {
+					rehabitation_center: rehabitation_center,
+					quater: quater
+				}
+		})
+		.then(response => {
+				if (response.data.code === 401) {
+						storage.removeStorage('token');
+						storage.removeStorage('role');
+						return response.data;
+				} else if (response.data.code === 200) {
+						return response.data;
+				}
+		}).catch(error => {
+				return error;
+		})
+	}
 }
 export default new Report();
--- src/views/ReportsParticipant/ReportsParticipant.js
+++ src/views/ReportsParticipant/ReportsParticipant.js
@@ -2,106 +2,148 @@ import React, { useState, useEffect } from 'react';
 import useStyles from './style';
 import { Alert } from 'components';
 import {
-	Button, Grid, Card, CircularProgress
+	Button, Grid, Card, CircularProgress, TextField
 } from '@material-ui/core';
 
-
+import XLSX from "xlsx-style-tw";
 import { Breadcrumb, SingleSelect } from 'components';
-import specialist from '../../apis/specialist';
-import clsx from 'clsx';
+import report from '../../apis/report';
+import { Autocomplete } from '@material-ui/lab';
 
 const ReportsParticipant = props => {
 	const { history } = props;
 	const classes = useStyles();
 
-	const breadcrumbs = [{ active: true, label: 'Specjaliści', href: '/specialists' }, { active: false, label: 'Dodaj specjalistę' }];
-	const [name, setName] = useState('');
-	const [qualification, setQualification] = useState(0);
-	const [qualificationList, setQualificationList] = useState([]);
-	const [specialty, setSpecialty] = useState(0);
-	const [specialtyList, setSpecialtyList] = useState([]);
+	const breadcrumbs = [{ active: true, label: 'Finanse', href: '/reports_service' }, { active: true, label: 'Raporty', href: '#' }, { active: false, label: 'Raport sprawozdawczy z rekrutacji uczestników' }];
+	const [name, setName] = useState('raport');
+	const [rehabitationCenter, setRehabitationCenter] = useState(0);
+	const [rehabitationCenterList, setRehabitationCenterList] = useState([]);
+	const [participant, setParticipant] = useState(null);
+	const [quater, setQuater] = useState([]);
+	const [quaterList, setQuaterList] = useState([]);
 	const [hasAlert, setHasAlert] = useState(false);
 	const [isSuccess, setIsSuccess] = useState(false);
 	const [message, setMessage] = useState('');
 	const [progressStatus, setProgressStatus] = useState(false);
+	const [result, setResult] = useState(null);
 	const [error, setError] = useState({});
 
 	useEffect(() => {
-		specialist.getInfo()
+		report.getServiceInfo()
 			.then(response => {
 				if (response.code === 401) {
 					history.push('/login');
 				} else {
-					setSpecialtyList(response.data.specialty);
-					setQualificationList(response.data.qualification);
+					setRehabitationCenterList(response.data.rehabitation_center);
 				}
 			})
 	}, []);
 
-	const handleError = () => {
-		let _error = {}
-		_error.name = (name.length === 0);
-		_error.qualification = (parseInt(qualification) === 0);
-		_error.specialty = (parseInt(specialty) === 0);
-		setError(_error);
-	};
-
-	const handleChangeName = (value) => {
-		setName(value);
-		let _error = JSON.parse(JSON.stringify(error));
-		_error.name = (value.length === 0);
-		setError(_error);
-	}
-
-	const handleChangeQualification = (value) => {
-		setQualification(value);
-		let _error = JSON.parse(JSON.stringify(error));
-		_error.qualification = (parseInt(value) === 0);
-		setError(_error);
-	}
-
-	const handleChangeSpecialty = (value) => {
-		setSpecialty(value);
-		let _error = JSON.parse(JSON.stringify(error));
-		_error.specialty = (parseInt(value) === 0);
-		setError(_error);
-	}
-
-	const checkError = () => {
-		return name.length === 0 || parseInt(qualification) === 0 || parseInt(specialty) === 0;
-	}
-
-
-	const handleBack = () => {
-		history.push('/specialists');
-	}
-
-	const handleSave = () => {
-
-
-		if (checkError()) {
-			setHasAlert(true);
-			setMessage('Proszę wypełnić wszystkie wymagane pola.');
-			setIsSuccess(false);
-			handleError();
-		} else {
-			setProgressStatus(true);
-
-			specialist.create(name, specialty, qualification)
+	useEffect(() => {
+		if (parseInt(rehabitationCenter) != 0) {
+			report.getServiceQuarter(rehabitationCenter)
 				.then(response => {
 					if (response.code === 401) {
 						history.push('/login');
 					} else {
-						setHasAlert(true);
-						setMessage(response.message);
-						setIsSuccess(response.code === 200);
-						if (response.code === 200) {
-							setTimeout(function () { history.push('/specialists'); }, 1000);
-						}
-						setProgressStatus(false);
+						let quater_list = response.data.quater;
+						quater_list = quater_list.map((item, index) => {
+							item.start_date = `Kwartał ${index + 1} (${item.start_date})`;
+							item.end_date = `Kwartał ${index + 1} (${item.end_date})`;
+							return item;
+						})
+						setQuaterList(quater_list);
 					}
 				})
 		}
+	}, [rehabitationCenter]);
+
+	useEffect(() => {
+		if (participant != null) {
+			report.getServiceQuarter(participant.rehabitation_center)
+				.then(response => {
+					if (response.code === 401) {
+						history.push('/login');
+					} else {
+						let quater_list = response.data.quater;
+						quater_list = quater_list.map((item, index) => {
+							item.start_date = `Kwartał ${index + 1} (${item.start_date})`;
+							item.end_date = `Kwartał ${index + 1} (${item.end_date})`;
+							return item;
+						})
+						setQuaterList(quater_list);
+					}
+				})
+		}
+	}, [participant]);
+
+	const handleChangeName = (value) => {
+		setName(value);
+	}
+
+	const handleChangeRehabitationCenter = (value) => {
+		setRehabitationCenter(value);
+		let _error = JSON.parse(JSON.stringify(error));
+		_error.rehabitation_center = (parseInt(value) === 0);
+		setError(_error);
+	}
+
+	const handleChangeQuater = (value) => {
+		setQuater(value);
+		let _error = JSON.parse(JSON.stringify(error));
+		_error.quater_from = (value === null);
+		_error.quater_to = (value === null);
+		setError(_error);
+	}
+
+	const checkError = () => {
+		return parseInt(rehabitationCenter) === 0
+	}
+
+	const handleGenerate = () => {
+		if (parseInt(rehabitationCenter) === 0 || !quater.id) {
+			setHasAlert(true);
+			setMessage('Proszę wypełnić wszystkie wymagane pola.');
+			setIsSuccess(false);
+		}
+		else {
+			setProgressStatus(true);
+			report.getRecruitmentData(rehabitationCenter, quater.id)
+				.then(response => {
+					if (response.code === 401) {
+						history.push('/login');
+					} else {
+						handleExport(response.data.count_list);
+					}
+					setProgressStatus(false);
+				})
+		}
+	}
+
+	const handleExport = (data) => {
+		let total_data = [];
+		total_data.push(['Instytucja rekrutująca',
+		'Liczba zakwalifikowanych uczestników']);
+		total_data.push(['PFRON', data[0]]);
+		total_data.push(['ZUS', data[1]]);
+		total_data.push(['Powiat', data[2]]);
+
+		const ws = XLSX.utils.aoa_to_sheet(total_data);
+		var wscols = [
+			{wch:40,alignment: {
+				wrapText: '1', // any truthy value here
+			},},
+			{wch:80, alignment: {
+				wrapText: '1', // any truthy value here
+			},},
+		];
+		
+		ws['!cols']	= wscols;
+		
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "SheetJS");
+    /* generate XLSX file and send to client */
+    XLSX.writeFile(wb, `${name}.xlsx`);
 	}
 
 	return (
@@ -109,10 +151,8 @@ const ReportsParticipant = props => {
 			<div className={classes.public}>
 				<div className={classes.controlBlock}>
 					<Breadcrumb list={breadcrumbs} />
-					<Button variant="outlined" color="secondary" id="main" className={classes.btnBack} onClick={handleBack}>          Wróć do listy specjalistów
-        </Button>
 				</div>
-				<Alert
+				<Alert 
 					hasAlert={hasAlert}
 					setHasAlert={setHasAlert}
 					isSuccess={isSuccess}
@@ -123,15 +163,51 @@ const ReportsParticipant = props => {
 						<Card className={classes.form}>
 							<Grid container spacing={3}>
 								<Grid item md={3} xs={12} className={classes.form_title}>
-									Dane podstawowe
-              </Grid>
+									Zdefiniuj dane raportu
+              	</Grid>
 								<Grid item md={9} xs={12}>
-									<div className={classes.top_label} ><label htmlFor="name">Tytuł, imię i nazwisko specjalisty</label></div>
-									<input className={clsx({ [classes.input_box]: true, [classes.error]: error.name })} id="name" type="name" value={name} name="name" onChange={(e) => handleChangeName(e.target.value)} />
-									<div className={classes.input_box_label} htmlFor="type">Wybierz punkt kwalifikacyjny</div>
-									<SingleSelect value={qualification} handleChange={(value) => handleChangeQualification(value)} list={qualificationList} error={error.qualification} />
-									<div className={classes.input_box_label} htmlFor="ambassador">Specjalność</div>
-									<SingleSelect value={specialty} handleChange={(value) => handleChangeSpecialty(value)} list={specialtyList} error={error.specialty} />
+									<div className={classes.top_label} ><label htmlFor="name">Nazwa raportu</label></div>
+									<input className={classes.input_box} id="name" type="name" value={name} name="name" onChange={(e) => handleChangeName(e.target.value)} />
+								</Grid>
+							</Grid>
+							<div className={classes.divide} />
+							<Grid container spacing={3}>
+								<Grid item md={3} xs={12} className={classes.form_title}>
+									Okres
+              	</Grid>
+								<Grid item md={9} xs={12}>
+									<Grid container spacing={2}>
+										<Grid item md={5} xs={12}>
+											<div className={classes.top_label} ><label htmlFor="rehabilitation_centers">Wybierz ORK</label></div>
+											<SingleSelect value={rehabitationCenter} handleChange={(value) => handleChangeRehabitationCenter(value)} list={rehabitationCenterList} />
+										</Grid>
+									</Grid>
+									<Grid container spacing={2}>
+										<Grid item md={4} xs={12}>
+											<div className={classes.top_label} ><label htmlFor="quater_from">Wybierz kwartał od</label></div>
+											<Autocomplete
+												className={classes.name_select_box}
+												id="quater_from"
+												onChange={(event, value) => handleChangeQuater(value)}
+												value={quater}
+												options={quaterList}
+												getOptionLabel={(option) => quaterList && option && option.start_date}
+												renderInput={(params) => <TextField {...params} variant="outlined" InputLabelProps={{ shrink: false }} />}
+											/>
+										</Grid>
+										<Grid item md={4} xs={12}>
+											<div className={classes.top_label} ><label htmlFor="quater_to">Wybierz kwartał do</label></div>
+											<Autocomplete
+												className={classes.name_select_box}
+												id="quater_to"
+												onChange={(event, value) => handleChangeQuater(value)}
+												value={quater}
+												options={quaterList}
+												getOptionLabel={(option) => quaterList && option && option.end_date}
+												renderInput={(params) => <TextField {...params} variant="outlined" InputLabelProps={{ shrink: false }} />}
+											/>
+										</Grid>
+									</Grid>
 								</Grid>
 							</Grid>
 						</Card>
@@ -140,9 +216,9 @@ const ReportsParticipant = props => {
 						<Card className={classes.form}>
 							<Grid container spacing={3}>
 								<Grid item xs={12}>
-									<Button variant="outlined" color="secondary" className={classes.btnSave} onClick={handleSave}>
-										Zapisz
-                </Button>
+									<Button variant="outlined" color="secondary" className={classes.btnSave} onClick={handleGenerate}>
+										Generuj raport
+                	</Button>
 								</Grid>
 							</Grid>
 						</Card>
